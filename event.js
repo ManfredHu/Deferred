6a907caf1abad22cacfa71dfbafe979085eefeee
manfred <manfredHuFE@gmail.com>
2016-07-29 00:54:16 +0800

    init event

---
+++ event.js
@@ -0,0 +1,66 @@
+/*
+ *定义一个事件对象，其下的handlers为多个类型的事件队列对象
+ */
+function event() {
+    this.handlers = {};
+}
+
+/*重写event的原型*/
+event.prototype = {
+    constructor: event, //维护contructor属性
+
+    /*监听函数*/
+    on: function(type, handler) {
+        /*如果没有这个类型的事件队列则创建一个*/
+        if (typeof this.handlers[type] == "undefined") {
+            this.handlers[type] = [];
+        }
+        /*添加事件到对应类型的事件队列里面*/
+        this.handlers[type].push(handler);
+    },
+
+    /*触发函数*/
+    emit: function(type) {
+        /*当handlers对象下面已经建立起对应类型的队列的时候*/
+        if (this.handlers[type] instanceof Array) {
+            var eventList = this.handlers[type];
+            var args = Array.prototype.slice.call(arguments,1);
+            /*执行所有已经添加到事件队列的函数*/
+            for (var i = 0, len = eventList.length; i < len; i++) {
+                eventList[i](args);
+            }
+        }
+    },
+
+    //只触发一次
+    once: function(type){
+        if (this.handlers[type] instanceof Array) {
+            var eventList = this.handlers[type];
+            var args = Array.prototype.slice.call(arguments,1);
+            /*执行所有已经添加到事件队列的函数*/
+            eventList[0](args);
+            delete this.handlers[type]; //也可以考虑直接删除这个属性
+        }
+    },
+
+    /*删除事件的函数*/
+    removeOne: function(type, handler) {
+        if (this.handlers[type] instanceof Array) {
+            var handlers = this.handlers[type];
+            for (var i = 0, len = handlers.length; i < len; i++) {
+                if (handlers[i] === handler) {
+                    break;
+                }
+            }
+            //删除第i个元素起的后面的1项
+            handlers.splice(i, 1);
+        }
+    },
+    
+    removeAll: function(type){
+        if (this.handlers[type] instanceof Array) {
+            this.handlers[type] = undefined;
+        }
+    }
+};
+
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Deffred异步模块简单实现
+
+## event.js
+订阅/发布模块的简单实现
+实现接口如下:
+
+- on: function(type, handler)
+- emit: function(type)
+- once: function(type)
+- removeOne: function(type, handler)
+
+type为事件类型，推荐使用.命名空间法，如个人中心模块`center`下的`showData`方法写为`center.showData`。
+
+- on为订阅方法
+- emit为广播方法
+- once只调用一次
+- removeOne删除对应监听的对应那个
+- removeAll对应监听的全部
+
+例子在`event测试文件夹里`
